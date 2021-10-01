937f6e3382d748aafe05d8a4d14c5b8d107fb2bf
Lahfir <nmhlahfir2@gmail.com>
2021-10-01 12:12:42 +0530

    22nd

--- static/Assets/JS/index.js
+++ static/Assets/JS/index.js
@@ -215,6 +215,38 @@ function thirdcontainer(e) {
     true
   );
 }
+function scheduleModal(e) {
+  var modal = document.getElementById("schedule-modal");
+  var _id = e.getAttribute("data-id");
+  // Get the <span> element that closes the modal
+  var span = document.getElementsByClassName("close")[4];
+
+  modal.style.display = "flex";
+
+  // When the user clicks on <span> (x), close the modal
+  span.onclick = function () {
+    modal.style.display = "none";
+  };
+
+  // When the user clicks anywhere outside of the modal, close it
+  window.onclick = function (event) {
+    if (event.target == modal) {
+      modal.style.display = "none";
+    }
+  };
+  window.addEventListener(
+    "keydown",
+    function (e) {
+      if (
+        (e.key == "Escape" || e.key == "Esc" || e.keyCode == 27) &&
+        e.target.nodeName == "BODY"
+      ) {
+        modal.style.display = "none";
+      }
+    },
+    true
+  );
+}
 function moreinfo() {
   var modal = document.getElementById("more-info-modal");
 
