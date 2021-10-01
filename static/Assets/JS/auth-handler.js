aa2626835b1eb171d31ddc5328ad1b0d440c88f8
Lahfir <nmhlahfir2@gmail.com>
2021-10-01 08:55:35 +0530

    13th

--- static/Assets/JS/auth-handler.js
+++ static/Assets/JS/auth-handler.js
@@ -2,10 +2,14 @@ $("#login-btn").click(function () {
   $("#login-btn").css({ "font-weight": "600", color: "white" });
   $("#register-btn").css({ "font-weight": "400", color: "black" });
   $("#switcher").css({ right: "unset", left: "6px", width: "85px" });
+  $("#register-form").hide();
+  $("#login-form").fadeIn(250);
 });
 
 $("#register-btn").click(function () {
   $("#register-btn").css({ "font-weight": "600", color: "white" });
   $("#switcher").css({ left: "unset", right: "6px", width: "110px" });
   $("#login-btn").css({ "font-weight": "400", color: "black" });
+  $("#login-form").hide();
+  $("#register-form").fadeIn(250);
 });
